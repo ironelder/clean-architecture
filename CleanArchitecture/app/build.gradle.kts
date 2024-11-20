00f3c54fc9998297d03ee64f1e36a07fda9df5df
장진혁/StoreClient팀/원스토어 <ironelder@onestorecorp.com>
2024-11-20 14:56:46 +0900

    hilt 빌드 속성 수정

--- CleanArchitecture/app/build.gradle.kts
+++ CleanArchitecture/app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.android.hilt)
     alias(libs.plugins.jetbrains.kotlin.kapt)
-    alias(libs.plugins.android.hilt) apply false
 }
 
 android {
@@ -59,10 +59,6 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.android.hilt)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     kapt(libs.android.hilt.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
