e395277c4c167c978a9eea2661d8bcc1c65a5add
장진혁/StoreClient팀/원스토어 <ironelder@onestorecorp.com>
2024-11-20 21:13:46 +0900

    사이드 메뉴 추가

--- CleanArchitecture/app/src/main/java/com/elder/cleanarchitecture/MainActivity.kt
+++ CleanArchitecture/app/src/main/java/com/elder/cleanarchitecture/MainActivity.kt
@@ -4,19 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.elder.cleanarchitecture.ui.navigation.BottomNavigationGraph
 import com.elder.cleanarchitecture.ui.navigation.BottomTabNavigation
 import com.elder.cleanarchitecture.ui.theme.CleanArchitectureTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -31,18 +51,67 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreenView() {
     val navController = rememberNavController()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
 
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        bottomBar = { BottomTabNavigation(navHostController = navController) }
-    ) { innerPadding ->
-        Box(modifier = Modifier.padding(innerPadding)) {
-            BottomNavigationGraph(navController = navController)
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        gesturesEnabled = true,
+        drawerContent = {
+            ModalDrawerSheet(modifier = Modifier.width(300.dp)) {
+                Text("Drawer title", modifier = Modifier.padding(16.dp))
+                HorizontalDivider()
+                NavigationDrawerItem(
+                    label = { Text(text = "Drawer Item") },
+                    selected = false,
+                    onClick = { }
+                )
+                NavigationDrawerItem(
+                    label = { Text(text = "Drawer Item2") },
+                    selected = false,
+                    onClick = { }
+                )
+                // ...other drawer items
+            }
+        }
+    ) {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+            },
+            bottomBar = { BottomTabNavigation(navHostController = navController) }
+        ) { innerPadding ->
+            Box(modifier = Modifier.padding(innerPadding)) {
+                BottomNavigationGraph(navController = navController)
+                FloatingActionButton(
+                    modifier = Modifier.background(Color.Transparent),
+                    elevation = FloatingActionButtonDefaults.elevation(
+                        defaultElevation = 0.dp, // 기본 그림자 제거
+                        pressedElevation = 0.dp, // 클릭 시 그림자 제거
+                        focusedElevation = 0.dp, // 포커스 상태 그림자 제거
+                        hoveredElevation = 0.dp // 호버 상태 그림자 제거
+                    ),
+                    containerColor = Color.Transparent,
+                    interactionSource = remember { MutableInteractionSource() },
+                    onClick = {
+                        scope.launch {
+                            if (drawerState.isOpen) {
+                                drawerState.close()
+                            } else {
+                                drawerState.open()
+                            }
+                        }
+                    }) {
+                    Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
+                }
+            }
         }
     }
+
 }
 
 @Composable
